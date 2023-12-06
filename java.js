75a6600cbcd0a16fea73f31f0ba373f9d4542162
leiladasilva <leilasilvasouza349@gmail.com>
2023-12-06 09:15:57 -0300

    tentei

--- java.js
+++ java.js
@@ -1,22 +1,78 @@
-const previousOperationText = document.querySelector("#previous-operation")
+var input = document.querySelector("#display");
+var operador = "";
+var parar = false;
 
-const currentOperationText = document.querySelector("#current-operation")
-
-const buttons = document.querySelector("#buttons-container button")
-
-class calculator{
-
+function limpar() {
+  input.value = "";
+  document.querySelector("#history").innerHTML = "";
+  operador = ""
 }
 
-buttons.forEach((btn) =>{
-    btn.addEventListener("click", (e) =>{
-        const value = e.target.value.innerText;
+function add_numero(string) {
+  if (!parar) {
+    if (string === '.' && input.value.indexOf(".") === -1 || string !== '.') {
+      input.value = input.value + string;
+      history.value = input.value;
+    }
+  } else {
+    if (string === '.' && input.value.indexOf(".") === -1 || string !== '.') {
+      input.value = "";
+      parar = false;
+      document.querySelector("#history").innerHTML = "";
+      operador = "";
 
-        
-        if(+value >= 0 || valeu === "."){
-        console.log(value);}
-        else{
-            console.log("Op")
-        }
-    });
-});
+      input.value = input.value + string;
+    }
+  }
+}
+
+function add_operador(op) {
+  if (input.value == "") {
+    input.value = "0";
+  }
+
+  if (operador == "") {
+    operador = op;
+    input.value = input.value + op;
+  } else {
+    total()
+    input.value = input.value + op;
+    operador = op;
+    parar=false;
+  }
+}
+
+function total() {
+  if (!parar) {
+    numeros = input.value.split(operador);
+
+    if (numeros[1] == "") {
+      input.value = input.value + '0';
+      numeros[1] = 0;
+
+    }
+
+    document.querySelector("#history").innerHTML = input.value;
+
+    switch (operador) {
+      case "+":
+        input.value = Number(numeros[0]) + Number(numeros[1]);
+        break;
+
+      case "-":
+        input.value = numeros[0] - numeros[1];
+        break;
+
+      case "*":
+        input.value = numeros[0] * numeros[1];
+        break;
+
+      case "/":
+        input.value = numeros[0] / numeros[1];
+        break;
+    }
+
+    parar = true;
+  }
+}
+
